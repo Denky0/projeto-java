4c2d4988086d02008a9296ecf4911df9865ed2d5
Veldrin <allangui619@gmail.com>
2025-08-20 22:31:28 -0300

    Homolog update

--- src/main/java/com/projetos_java/dslist/services/GameListService.java
+++ src/main/java/com/projetos_java/dslist/services/GameListService.java
@@ -8,9 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.projetos_java.dslist.dto.GameListDTO;
 import com.projetos_java.dslist.entities.GameList;
-import com.projetos_java.dslist.projections.GameMinProjection;
 import com.projetos_java.dslist.repositories.GameListRepository;
-import com.projetos_java.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -18,8 +16,6 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
-	@Autowired
-	private GameRepository gameRepository;
 	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
@@ -27,21 +23,6 @@ public class GameListService {
 		return result.stream().map(GameListDTO::new).toList();
 	}
 	
-	@Transactional
-	public void move(Long listId, int sourceIndex, int destinationIndex) {
-
-		List<GameMinProjection> list = gameRepository.searchByList(listId);
-
-		GameMinProjection obj = list.remove(sourceIndex);
-		list.add(destinationIndex, obj);
-
-		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
-		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
-
-		for (int i = min; i <= max; i++) {
-			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
-		}
-	}
 
 	@Transactional(readOnly = true)
 	public GameListDTO findById(Long id) {
